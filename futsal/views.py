13bf7df6dc9573f59f4f947bbc09b2d00b2180d3
KedarBaral <kedarbaral47@gmail.com>
2019-02-17 20:21:38 +0545

    initial commit

--- futsal/views.py
+++ futsal/views.py
@@ -3,3 +3,7 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, "futsal/index.html", {})
+
+
+def detail(request):
+    return render(request, "futsal/detail.html", {})
